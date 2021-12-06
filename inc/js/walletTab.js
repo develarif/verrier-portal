ad2b9feeade0d0bceda358265c51ea3858aa7e5a
sikdermdariff <sikdermohammadarif@gmail.com>
2021-12-06 17:25:25 +0600

    Billing page done

--- inc/js/walletTab.js
+++ inc/js/walletTab.js
@@ -1,14 +1,36 @@
-const getTabUl = document.querySelector('#wallet-tab-header');
-const allTabBtn = document.querySelectorAll('#wallet-tab-header > li');
-const tabBody = document.querySelector('.wallet-tab-body');
-const allTabItem = document.querySelectorAll('.tab-item');
 
-for (let i = 0; i < allTabBtn.length; i++) {
-    allTabBtn[i].addEventListener('click', ()=> {
-        getTabUl.getElementsByClassName('active')[0].classList.remove('active');
-        allTabBtn[i].classList.add('active');
+const walletTabFunc = () => {
+    const getTabUl = document.querySelector('#wallet-tab-header');
+    const allTabBtn = document.querySelectorAll('#wallet-tab-header > li');
+    const tabBody = document.querySelector('.wallet-tab-body');
+    const allTabItem = document.querySelectorAll('.tab-item');
 
-        tabBody.getElementsByClassName('active')[0].classList.remove('active');
-        allTabItem[i].classList.add('active');
-    });
+    for (let i = 0; i < allTabBtn.length; i++) {
+        allTabBtn[i].addEventListener('click', ()=> {
+            getTabUl.getElementsByClassName('active')[0].classList.remove('active');
+            allTabBtn[i].classList.add('active');
+
+            tabBody.getElementsByClassName('active')[0].classList.remove('active');
+            allTabItem[i].classList.add('active');
+        });
+    }
 }
+walletTabFunc();
+
+const billingTabFunc = () => {
+    const getTabUl = document.querySelector('#billing-tab-ui');
+    const allTabBtn = document.querySelectorAll('#billing-tab-ui > li');
+    const tabBody = document.querySelector('.billing-tab-body');
+    const allTabItem = document.querySelectorAll('.billing-tab-item');
+
+    for (let i = 0; i < allTabBtn.length; i++) {
+        allTabBtn[i].addEventListener('click', ()=> {
+            getTabUl.getElementsByClassName('active')[0].classList.remove('active');
+            allTabBtn[i].classList.add('active');
+
+            tabBody.getElementsByClassName('active')[0].classList.remove('active');
+            allTabItem[i].classList.add('active');
+        });
+    }
+}
+billingTabFunc();
