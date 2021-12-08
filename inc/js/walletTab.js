f3d01a79c9f3ace76939605c3d39000ed395b480
sikdermdariff <sikdermohammadarif@gmail.com>
2021-12-08 18:01:45 +0600

    Settings page done except 2 tab

--- inc/js/walletTab.js
+++ inc/js/walletTab.js
@@ -34,3 +34,22 @@ const billingTabFunc = () => {
     }
 }
 billingTabFunc();
+
+
+const settingTabFunc = () => {
+    const getTabUl = document.querySelector('#setting-tab-header');
+    const allTabBtn = document.querySelectorAll('#setting-tab-header > li');
+    const tabBody = document.querySelector('.setting-tab-body');
+    const allTabItem = document.querySelectorAll('.tab-item');
+
+    for (let i = 0; i < allTabBtn.length; i++) {
+        allTabBtn[i].addEventListener('click', ()=> {
+            getTabUl.getElementsByClassName('active')[0].classList.remove('active');
+            allTabBtn[i].classList.add('active');
+
+            tabBody.getElementsByClassName('active')[0].classList.remove('active');
+            allTabItem[i].classList.add('active');
+        });
+    }
+}
+settingTabFunc();
